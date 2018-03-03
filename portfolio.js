f35482576ddc9e031c6baded971902a64acde551
Colin Davidson <colrdavidson@gmail.com>
2018-03-03 02:31:21 -0500

    Update and cleanup portfolio page

--- portfolio.js
+++ portfolio.js
@@ -2,6 +2,4 @@ function fill_email() {
 	var addr = "Y29scmRhdmlkc29uQGdtYWlsLmNvbQ==";
 	document.getElementById("emaila").innerHTML = atob(addr);
 	document.getElementById("emaila").href = "mailto:" + atob(addr);
-	document.getElementById("emaila2").innerHTML = atob(addr);
-	document.getElementById("emaila2").href = "mailto:" + atob(addr);
 }
