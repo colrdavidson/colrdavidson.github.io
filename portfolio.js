9d1a6a63e569c39587cdcb6a5600abe7c1851927
Colin Davidson <colrdavidson@gmail.com>
2023-01-01 21:03:58 -0800

    remove bootstrap

--- portfolio.js
+++
@@ -1,5 +0,0 @@
-function fill_email() {
-	var addr = "Y29scmRhdmlkc29uQGdtYWlsLmNvbQ==";
-	document.getElementById("emaila").innerHTML = atob(addr);
-	document.getElementById("emaila").href = "mailto:" + atob(addr);
-}
